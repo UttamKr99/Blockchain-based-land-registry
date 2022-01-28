cfdfcd11ef06d069ee54b2a903c35ca288c18778
carnotojee <carnotojee@gmai.com>
2022-01-28 21:59:51 +0530

    Add <Vishal Yadav> to Contributors list

--- Client/src/Layouts/Register.Layout.js
+++ Client/src/Layouts/Register.Layout.js
@@ -1,31 +1,38 @@
 import React, { useState } from 'react';
 import RegisterPage from './Register';
 function RegisterLayout() {
-  const [role, setRole] = useState('');
-  const modleHandler = () => {};
+  const [role, setRole] = useState("Choose");
+  const [set, superset] = useState(false);
+  const toggle = () => {
+    superset(!set);
+  };
+  const modleHandler = (e) => {
+    setRole(e.target.value);
+  };
+
   return (
     <>
-      <div class='modal'>
-        <div class='modal-background'></div>
-        <div class='modal-card'>
-          <header class='modal-card-head'>
-            <p class='modal-card-title'>Select Your Role</p>
+      <div class={!set ? "modal is-active" : "modal"}>
+        <div class="modal-background"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Select Your Role</p>
           </header>
-          <section class='modal-card-body'>
-            <div className='is-flex is-justify-content-center'>
-              <div class='select'>
+          <section class="modal-card-body">
+            <div className="is-flex is-justify-content-center">
+              <div class="select" value={role} onChange={modleHandler}>
                 <select>
-                  <option>Choose</option>
-                  <option value=''>Seller</option>
-                  <option value=''>Buyer</option>
+                  <option selected>Choose</option>
+                  <option value="Seller">Seller</option>
+                  <option value="Buyer">Buyer</option>
                 </select>
               </div>
             </div>
           </section>
-          <footer class='modal-card-foot'>
-            <button class='button is-success' onClick={modleHandler}>
+          <footer class="modal-card-foot">
+            <button class="button is-success" onClick={toggle}>
               Done
-            </button>
+            </button>{" "}
           </footer>
         </div>
       </div>
--- Client/src/Layouts/Register.js
+++ Client/src/Layouts/Register.js
@@ -1,108 +1,108 @@
 function Register() {
   return (
     <>
-      <div class='columns'>
-        <div class='column is-two-fifths mx-auto'>
+      <div class="columns">
+        <div class="column is-two-fifths mx-auto">
           <form>
-            <div class='field'>
-              <label class='label'>Full Name</label>
-              <div class='control'>
-                <input class='input' type='text' placeholder='e.g Alex Smith' />
+            <div class="field">
+              <label class="label">Full Name</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="e.g Alex Smith" />
               </div>
             </div>
-            <div class='field'>
-              <label class='label'>Email</label>
-              <div class='control'>
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
                 <input
-                  class='input'
-                  type='email'
-                  placeholder='e.g. alexsmith@gmail.com'
+                  class="input"
+                  type="email"
+                  placeholder="e.g. alexsmith@gmail.com"
                 />
               </div>
             </div>
-            <div class='field'>
-              <label class='label'>Phone No.</label>
+            <div class="field">
+              <label class="label">Phone No.</label>
 
-              <div class='field has-addons'>
-                <p class='control'>
-                  <button class='button is-static'>+91</button>
+              <div class="field has-addons">
+                <p class="control">
+                  <button class="button is-static">+91</button>
                 </p>
-                <p class='control is-expanded'>
+                <p class="control is-expanded">
                   <input
-                    class='input'
-                    type='tel'
-                    placeholder='Your 10 digit  phone number'
+                    class="input"
+                    type="tel"
+                    placeholder="Your 10 digit  phone number"
                   />
                 </p>
               </div>
-              <p class='help'>Don't enter +91 with phone no.</p>
+              <p class="help">Don't enter +91 with phone no.</p>
             </div>
-            <div class='field'>
-              <label class='label'>Date-Of-Birth</label>
-              <div class='control'>
+            <div class="field">
+              <label class="label">Date-Of-Birth</label>
+              <div class="control">
                 <input
-                  class='input'
-                  type='text'
-                  placeholder='e.g. alexsmith@gmail.com'
+                  class="input"
+                  type="text"
+                  placeholder="e.g. alexsmith@gmail.com"
                 />
               </div>
             </div>
-            <div class='field'>
-              <label class='label'>Full Address </label>
-              <div class='control'>
+            <div class="field">
+              <label class="label">Full Address </label>
+              <div class="control">
                 <input
-                  class='input'
-                  type='text'
-                  placeholder='e.g. Mahatma Gandhi Road, Pune, Maharashtra'
+                  class="input"
+                  type="text"
+                  placeholder="e.g. Mahatma Gandhi Road, Pune, Maharashtra"
                 />
               </div>
             </div>
-            <div class='field'>
-              <label class='label'>PIN CODE</label>
-              <div class='control'>
+            <div class="field">
+              <label class="label">PIN CODE</label>
+              <div class="control">
                 <input
-                  class='input'
-                  type='text'
-                  placeholder='e.g. 220000'
+                  class="input"
+                  type="number"
+                  placeholder="e.g. 220000"
                   required
                 />
               </div>
-              <div class='field'>
-                <label class='label'>Aadhar No.</label>
-                <div class='control'>
+              <div class="field">
+                <label class="label">Aadhar No.</label>
+                <div class="control">
                   <input
-                    class='input'
-                    type='email'
-                    placeholder='e.g. 123456789000'
+                    class="input"
+                    type="tel"
+                    placeholder="e.g. 123456789000"
                   />
                 </div>
               </div>
-              <div class='field'>
-                <label class='label'>PAN No.</label>
-                <div class='control'>
+              <div class="field">
+                <label class="label">PAN No.</label>
+                <div class="control">
                   <input
-                    class='input'
-                    type='email'
-                    placeholder='e.g. PAN1234567'
+                    class="input"
+                    type="text"
+                    placeholder="e.g. PAN1234567"
                   />
                 </div>
-                <div class='field'>
-                  <label class='label'>Upload Aadhar(.pdf)</label>
-                  <div class='file has-name'>
-                    <label class='file-label'>
-                      <input class='file-input' type='file' name='resume' />
-                      <span class='file-cta'>
-                        <span class='file-icon'>
-                          <i class='fas fa-upload'></i>
+                <div class="field">
+                  <label class="label">Upload Aadhar(.pdf)</label>
+                  <div class="file has-name">
+                    <label class="file-label">
+                      <input class="file-input" type="file" name="resume" />
+                      <span class="file-cta">
+                        <span class="file-icon">
+                          <i class="fas fa-upload"></i>
                         </span>
-                        <span class='file-label'>Choose a file…</span>
+                        <span class="file-label">Choose a file…</span>
                       </span>
-                      <span class='file-name'></span>
+                      <span class="file-name"></span>
                     </label>
                   </div>
                 </div>
-                <p class='control'>
-                  <button className='button is-success'>Submit</button>
+                <p class="control">
+                  <button className="button is-success">Submit</button>
                 </p>
               </div>
             </div>
