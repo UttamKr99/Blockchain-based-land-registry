231390bdb7707803899bb1bee57cb9cc3070468a
Uttam Kumar Singh <uttamkumarsingh99@gmail.com>
2022-01-31 22:37:59 +0530

    first commite

--- Client/src/Layouts/Register.js
+++ Client/src/Layouts/Register.js
@@ -7,7 +7,7 @@ function Register() {
             <div class="field">
               <label class="label">Full Name</label>
               <div class="control">
-                <input class="input" type="text" placeholder="e.g Alex Smith" />
+                <input class="input" type="text" placeholder="Alex Smith" />
               </div>
             </div>
             <div class="field">
