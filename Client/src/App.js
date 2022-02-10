3cb1a9db3669d09805c61779619c8a7e3314efa3
veeresh.maurya <veeresh.maurya@impledge.com>
2022-02-10 11:25:27 +0530

    Some Updates

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,16 +1,15 @@
-import './App.css'
-import Homepage from './Layouts/Homepage'
-import { Navbar } from './Layouts/Navbar'
-import Login from './Layouts/Register.Layout'
+import './App.css';
+import Homepage from './Layouts/Homepage';
+import { Navbar } from './Layouts/Navbar';
+import Login from './Layouts/Register.Layout';
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Login />
-      
-    </div>
-  ) // <Homepage />
-  //
+     return (
+          <div className="App">
+               <Navbar />
+               <Login />
+          </div>
+     ); // <Homepage />
+     //
 }
 
-export default App
+export default App;
